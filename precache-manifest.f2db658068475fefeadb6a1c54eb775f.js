1afee7203e3b68d8cd5fb84265f68959798a16d7
ApoorvSadana <apoorvsadana@users.noreply.github.com>
2021-04-14 03:56:56 +0530

    Updates

---
+++ precache-manifest.f2db658068475fefeadb6a1c54eb775f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bed7e968d383f0f9c98257c98f7e032",
+    "url": "/blockchain-project/index.html"
+  },
+  {
+    "revision": "59cbb759ad14ca68ffa3",
+    "url": "/blockchain-project/static/css/2.e53b351e.chunk.css"
+  },
+  {
+    "revision": "2c6d9c82261af5ae7777",
+    "url": "/blockchain-project/static/css/main.56d4368f.chunk.css"
+  },
+  {
+    "revision": "59cbb759ad14ca68ffa3",
+    "url": "/blockchain-project/static/js/2.fcf19f9b.chunk.js"
+  },
+  {
+    "revision": "59f009152346bede7537573cca7aafc0",
+    "url": "/blockchain-project/static/js/2.fcf19f9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c6d9c82261af5ae7777",
+    "url": "/blockchain-project/static/js/main.09f75bee.chunk.js"
+  },
+  {
+    "revision": "fe2db79c52900be168bf",
+    "url": "/blockchain-project/static/js/runtime-main.c829d3f5.js"
+  },
+  {
+    "revision": "f374e2f5d3f464f42edeacddffcbb1c2",
+    "url": "/blockchain-project/static/media/profilePic.f374e2f5.jpg"
+  }
+]);
